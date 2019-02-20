2b7a81056b006d200ecd55225d80d021890df673
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-19 22:00:05 -0300

    Descomentando...

--- 03-events/index.js
+++ 03-events/index.js
@@ -9,13 +9,13 @@ meuEmissor.on(nomeEvento, function (click) {
     console.log('O usuário clicou', click)
 })
 
-/* meuEmissor.emit(nomeEvento, 'na barra de rolagem')
+meuEmissor.emit(nomeEvento, 'na barra de rolagem')
 meuEmissor.emit(nomeEvento, 'no botão de ok')
 
 let count = 0;
 setInterval(function () {
     meuEmissor.emit(nomeEvento, 'no painel ' + (count++))
-}, 1000) */
+}, 1000)
 
 const stdin = process.openStdin()
 stdin.addListener('data', function (value) {
