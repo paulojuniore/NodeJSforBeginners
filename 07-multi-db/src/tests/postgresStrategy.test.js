c63bd65e24c158a1b548f42d42c59c19cb68c90a
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-02-24 18:20:33 -0300

    Criando o teste para a operação CREATE no banco de dados

--- 07-multi-db/src/tests/postgresStrategy.test.js
+++ 07-multi-db/src/tests/postgresStrategy.test.js
@@ -4,10 +4,19 @@ const Context = require('../db/strategies/base/contextStrategy')
 
 const context = new Context(new Postgres)
 
+const MOCK_HEROI_CADASTRAR = {
+    nome: 'Gavião Negro',
+    poder: 'Flechas'
+}
+
 describe('Postgres Strategy', function () {
     this.timeout(Infinity)
     it('Postgres Connection', async function () {
         const result = await context.isConnected()
         assert.equal(result, true)
+    })
+    it('Create a register in the table heroes', async function () {
+        const result = await context.create(MOCK_HEROI_CADASTRAR)
+        assert.deepEqual(result, MOCK_HEROI_CADASTRAR)
     })
 })
